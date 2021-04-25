bf7936918c330749af91e17c5fa4164941eddc36
BlandineDupas <blandine.dupas@sfr.fr>
2021-04-25 14:49:00 +0200

    delete friendspage slice

--- src/app/store.js
+++ src/app/store.js
@@ -3,7 +3,6 @@ import userReducer from 'reducers/user/userSlice';
 import postFormReducer from '../features/postForm/postFormSlice';
 import homeReducer from '../features/home/homeSlice';
 import commentFormReducer from '../features/commentForm/commentFormSlice';
-import friendsPageReducer from '../components/main/friendsPage/friendsPageSlice';
 
 export const store = configureStore({
   reducer: {
@@ -11,6 +10,5 @@ export const store = configureStore({
     postForm: postFormReducer,
     home: homeReducer,
     commentForm: commentFormReducer,
-    friendsPage: friendsPageReducer
   },
 });
